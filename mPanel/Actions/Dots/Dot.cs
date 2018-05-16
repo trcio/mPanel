f33d7ce803701e0d92c21a7d06f8bb9609829023
pat <pat@patpositron.com>
2018-05-16 13:51:29 -0500

    refactor

--- mPanel/Actions/Dots/Dot.cs
+++ mPanel/Actions/Dots/Dot.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using mPanel.Extra;
 using mPanel.Matrix;
+using mPanel.Extra;
 
 namespace mPanel.Actions.Dots
 {
--- mPanel/Actions/Dots/DotsForm.cs
+++ mPanel/Actions/Dots/DotsForm.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Timers;
-using mPanel.Extra;
 using Timer = System.Timers.Timer;
 using mPanel.Matrix;
+using mPanel.Extra;
 
 namespace mPanel.Actions.Dots
 {
